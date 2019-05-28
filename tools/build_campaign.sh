894b836b37f36caf7d57b24936121fda67af0276
Beha <shacknetisp@mail.com>
2019-05-27 21:41:28 -0400

    Optional ICON.

--- tools/build_campaign.sh
+++ tools/build_campaign.sh
@@ -3,4 +3,7 @@ cp _server.pbl.in _server.pbl || exit 1
 sed -i "s/@PASSPHRASE@/$(cat secrets/passphrase.ign)/g" _server.pbl || exit 1
 sed -i "s/@EMAIL@/$(cat secrets/email.ign)/g" _server.pbl || exit 1
 sed -i "s/@VERSION@/$(cat VERSION)/g" _server.pbl || exit 1
-sed -i "s#@ICON@#data:image/png;base64,$(base64 -w 0 icon.png)#g" _server.pbl || exit 1
+
+if [ -e icon.png ]; then
+	sed -i "s#@ICON@#data:image/png;base64,$(base64 -w 0 icon.png)#g" _server.pbl || exit 1
+fi

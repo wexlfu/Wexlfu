4b2f9b58de391765223c4e82f60cb444ef905755
Beha <shacknetisp@mail.com>
2019-05-27 21:59:36 -0400

    description.txt

--- tools/build_campaign.sh
+++ tools/build_campaign.sh
@@ -4,6 +4,10 @@ sed -i "s/@PASSPHRASE@/$(cat secrets/passphrase.ign)/g" _server.pbl || exit 1
 sed -i "s/@EMAIL@/$(cat secrets/email.ign)/g" _server.pbl || exit 1
 sed -i "s/@VERSION@/$(cat VERSION)/g" _server.pbl || exit 1
 
+if [ -e description.txt ]; then
+	sed -i "s#@DESCRIPTION@#$(cat description.txt)#g" _server.pbl || exit 1
+fi
+
 if [ -e icon.png ]; then
 	sed -i "s#@ICON@#data:image/png;base64,$(base64 -w 0 icon.png)#g" _server.pbl || exit 1
 fi

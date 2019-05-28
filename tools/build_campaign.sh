694258e80ce82e84c6084a8cfa862dd7520db42d
Beha <shacknetisp@mail.com>
2019-05-27 22:13:06 -0400

    Better tools.

--- tools/build_campaign.sh
+++ tools/build_campaign.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-cp _server.pbl.in _server.pbl || exit 1
-sed -i "s/@PASSPHRASE@/$(cat secrets/passphrase.ign)/g" _server.pbl || exit 1
-sed -i "s/@EMAIL@/$(cat secrets/email.ign)/g" _server.pbl || exit 1
-sed -i "s/@VERSION@/$(cat VERSION)/g" _server.pbl || exit 1
+set -ex
+
+cp _server.pbl.in _server.pbl
+sed -i "s/@PASSPHRASE@/$(cat secrets/passphrase.ign)/g" _server.pbl
+sed -i "s/@EMAIL@/$(cat secrets/email.ign)/g" _server.pbl
+sed -i "s/@VERSION@/$(cat VERSION)/g" _server.pbl
 
 if [ -e description.txt ]; then
-	sed -i "s#@DESCRIPTION@#$(cat description.txt)#g" _server.pbl || exit 1
+	sed -i "s#@DESCRIPTION@#$(cat description.txt)#g" _server.pbl
 fi
 
 if [ -e icon.png ]; then
-	sed -i "s#@ICON@#data:image/png;base64,$(base64 -w 0 icon.png)#g" _server.pbl || exit 1
+	sed -i "s#@ICON@#data:image/png;base64,$(base64 -w 0 icon.png)#g" _server.pbl
 fi
--- tools/build_wexlfu.sh
+++ tools/build_wexlfu.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-cp loader.cfg.in loader.cfg || exit 1
-sed -i "s/@VERSION@/$(cat VERSION)/g" loader.cfg || exit 1
+set -ex
+
+cp loader.cfg.in loader.cfg
+sed -i "s/@VERSION@/$(cat VERSION)/g" loader.cfg

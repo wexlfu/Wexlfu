0e03ec0bf6282a458cefb814c15dd1568adf9cbf
Beha <shacknetisp@mail.com>
2019-05-31 12:00:09 -0400

    put_unit_vacant docs

--- lua/map.lua
+++ lua/map.lua
@@ -1,5 +1,7 @@
 M = {}
 
+-- Put unit, find next vacant if needed.
+-- x and y are optional if unit contains coordinates already.
 function M.put_unit_vacant(unit, x, y)
 	local x = x or unit.x
 	local y = y or unit.y

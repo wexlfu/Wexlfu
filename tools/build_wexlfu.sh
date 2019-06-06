60a4fe0dd320914a213e7b8ad135627e45c39406
Beha <shacknetisp@mail.com>
2019-06-05 22:00:42 -0400

    New textdomain.

--- tools/build_wexlfu.sh
+++ tools/build_wexlfu.sh
@@ -4,3 +4,5 @@ set -ex
 cp version.cfg.in version.cfg
 sed -i "s/@VERSION@/$(cat VERSION)/g" version.cfg
 sed -i "s/@MAJOR@/$(cat VERSION | cut -d. -f1)/g" version.cfg
+
+find -name '*.cfg' | xargs sed -i "s/^#textdomain wesnoth-Wexlfu-.*$/#textdomain wesnoth-Wexlfu-$(cat VERSION)/g"
